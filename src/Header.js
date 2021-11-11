edf1c9402b99a08c33a2f63b164255799e9652b3
Spencer-Lake <spencer.m.lake11@gmail.com>
2021-11-11 11:12:58 -0500

    deleted icon

---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Header.css'
+
+
+export default function Header() {
+    return(
+        <div className="header">
+            <nav className="hamburger-menu">
+                <ul id="menu">
+                    <li><a href="./addVide">Add Vibe</a></li>
+                    <li>Sort Vibes</li>
+                    <li>Log Out</li>
+                </ul>
+            </nav>
+            <p id="username">Username</p>
+        </div>
+    )
+}
